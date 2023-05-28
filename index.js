8ab06fe8fe7b2d9e8ba4c575ff96f5209f404cb4
Shanna Salcedo <salcedos@ufl.edu>
2023-05-27 23:54:30 -0500

    final updates

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ function greet() {
 
     var age = prompt('How old are you?'); console.log(parseInt(age))
 
-    var birthday = confirm('Has your birthday passed this year?') 
+    var birthday = confirm('Has your birthday passed this year?'); if (birthday === true) { alert( 'You were born in the year ' + (2023 - age))
+
+         } else { alert ('You were born in the year ' + ((2023 - 1) - age))
+        
+    }
+    const today = new Date('May 27, 23');
+
+console.log(today.getFullYear());
 
 }
