7b1aee513266d8e11f5371894a5dce5b2dbb79d0
Shanna Salcedo <salcedos@ufl.edu>
2023-05-27 23:17:32 -0500

    updates

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Your code here
+function greet() {
+
+    var name = prompt('What is your name?'); alert('Hello, ' + name)
+
+    var age = prompt('How old are you?'); console.log(parseInt(age))
+
+    var birthday = confirm('Has your birthday passed this year?') 
+
+}
